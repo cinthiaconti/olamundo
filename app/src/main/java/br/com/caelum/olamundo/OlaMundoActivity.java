fee9ad19d824bc92c12cf5427d0ecd09ec1a56c5
Cinthia Conti <cinthiaconti@gmail.com>
2018-10-18 21:22:14 -0300

    first commit

---
+++ app/src/main/java/br/com/caelum/olamundo/OlaMundoActivity.java
@@ -0,0 +1,17 @@
+package br.com.caelum.olamundo;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Cinthia on 06/02/2016.
+ */
+public class OlaMundoActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState){
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ola_mundo);
+    }
+}
